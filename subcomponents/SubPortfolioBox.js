5af085ad18b41a94cf3d3df818f179a6be7e40fc
Saqib Tanveer <adward797@gmail.com>
2023-08-06 11:15:35 -0700

    seted portfolio section of first page

--- subcomponents/SubPortfolioBox.js
+++ subcomponents/SubPortfolioBox.js
@@ -5,7 +5,6 @@ export default function SubPortfolioBox({imageUrl, title, subTitle, description}
   return (
     <div className="lg:w-1/3 sm:w-1/2 p-4">
     <div className="flex relative">
-      {/* <img alt="gallery" className="absolute inset-0 w-full h-full object-cover object-center" src="https://dummyimage.com/600x360"/> */}
       <Image alt='Portfolio Image' src={imageUrl} height={300} width={300} className="absolute inset-0 w-full h-full object-contain object-center" />
       <div className="px-8 py-10 relative z-10 w-full border-4 border-gray-200 bg-white opacity-0 hover:opacity-100">
         <h2 className="tracking-widest text-sm title-font font-medium text-indigo-500 mb-1">THE SUBTITLE</h2>
