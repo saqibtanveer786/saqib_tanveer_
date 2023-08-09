91f7dd60e903588ede3a58d43d004c3046900dfc
Saqib Tanveer <adward797@gmail.com>
2023-08-09 00:50:23 -0700

    solve over padding issue

--- components/Portfolio.js
+++ components/Portfolio.js
@@ -15,36 +15,10 @@ export default function Portfolio() {
       <p className="lg:w-2/3 mx-auto leading-relaxed text-base">Here are the projects I have worked on.</p>
     </div>
     <div className="flex flex-wrap -m-4">
-    <div className="lg:w-1/3 sm:w-1/2 p-4">
-    <div className="flex relative">
-      <Image alt='Portfolio Image' src={'/portfolioImages/mohizBai.png'} height={300} width={300} className="absolute inset-0 w-full h-full object-contain object-center" />
-      <div className="px-8 py-10 relative z-10 w-full border-4 border-gray-200 bg-white opacity-0 hover:opacity-100">
-        <h2 className="tracking-widest text-sm title-font font-medium text-indigo-500 mb-1">THE SUBTITLE</h2>
-        <h1 className="title-font text-lg font-medium text-gray-900 mb-3">Shooting Stars</h1>
-        <p className="leading-relaxed">Photo booth fam kinfolk cold-pressed sriracha leggings jianbing microdosing tousled waistcoat.</p>
-      </div>
-    </div>
-    </div>
-    <div className="lg:w-1/3 sm:w-1/2 p-4">
-    <div className="flex relative">
-      <Image alt='Portfolio Image' src={'/portfolioImages/front.png'} height={300} width={300} className="absolute inset-0 w-full h-full object-contain object-center" />
-      <div className="px-8 py-10 relative z-10 w-full border-4 border-gray-200 bg-white opacity-0 hover:opacity-100">
-        <h2 className="tracking-widest text-sm title-font font-medium text-indigo-500 mb-1">THE SUBTITLE</h2>
-        <h1 className="title-font text-lg font-medium text-gray-900 mb-3">Shooting Stars</h1>
-        <p className="leading-relaxed">Photo booth fam kinfolk cold-pressed sriracha leggings jianbing microdosing tousled waistcoat.</p>
-      </div>
-    </div>
-    </div>
-    <div className="lg:w-1/3 sm:w-1/2 p-4">
-    <div className="flex relative">
-      <Image alt='Portfolio Image' src={'/portfolioImages/calculator.png'} height={300} width={300} className="absolute inset-0 w-full h-full object-contain object-center" />
-      <div className="px-8 py-10 relative z-10 w-full border-4 border-gray-200 bg-white opacity-0 hover:opacity-100">
-        <h2 className="tracking-widest text-sm title-font font-medium text-indigo-500 mb-1">THE SUBTITLE</h2>
-        <h1 className="title-font text-lg font-medium text-gray-900 mb-3">Shooting Stars</h1>
-        <p className="leading-relaxed">Photo booth fam kinfolk cold-pressed sriracha leggings jianbing microdosing tousled waistcoat.</p>
-      </div>
-    </div>
-    </div>
+      <SubPortfolioBox imageUrl={'/portfolioImages/mohizBai.png'} title={'React.js Portfolio Website'} subTitle={'build in react'} description={'This is a website develop by using React.js. Fully responsive and amazing website'}/>
+      <SubPortfolioBox imageUrl={'/portfolioImages/portfolioItem2.png'} title={'React.js Portfolio Website'} subTitle={'build in react'} description={'This is a website develop by using React.js. Fully responsive and amazing website'}/>
+      <SubPortfolioBox imageUrl={'/portfolioImages/portfolioItem3.png'} title={'React.js Portfolio Website'} subTitle={'build in react'} description={'This is a website develop by using React.js. Fully responsive and amazing website'}/>
+      <SubPortfolioBox imageUrl={'/portfolioImages/portfolioItem4.png'} title={'React.js Portfolio Website'} subTitle={'build in react'} description={'This is a website develop by using React.js. Fully responsive and amazing website'}/>
     </div>
   </div>
 </section>
--- subcomponents/SubPortfolioBox.js
+++ subcomponents/SubPortfolioBox.js
@@ -6,10 +6,10 @@ export default function SubPortfolioBox({imageUrl, title, subTitle, description}
     <div className="lg:w-1/3 sm:w-1/2 p-4">
     <div className="flex relative">
       <Image alt='Portfolio Image' src={imageUrl} height={300} width={300} className="absolute inset-0 w-full h-full object-contain object-center" />
-      <div className="px-8 py-10 relative z-10 w-full border-4 border-gray-200 bg-white opacity-0 hover:opacity-100">
-        <h2 className="tracking-widest text-sm title-font font-medium text-indigo-500 mb-1">THE SUBTITLE</h2>
-        <h1 className="title-font text-lg font-medium text-gray-900 mb-3">Shooting Stars</h1>
-        <p className="leading-relaxed">Photo booth fam kinfolk cold-pressed sriracha leggings jianbing microdosing tousled waistcoat.</p>
+      <div className="px-8 py-6 md:py-10 relative z-10 w-full border-4 border-gray-200 bg-white opacity-0 hover:opacity-100">
+        <h2 className="tracking-widest text-sm title-font font-medium text-indigo-500 mb-1">{subTitle}</h2>
+        <h1 className="title-font text-lg font-medium text-gray-900 mb-3">{title}</h1>
+        <p className="leading-relaxed">{description}</p>
       </div>
     </div>
     </div>
