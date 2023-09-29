f97afaa5878bb22eb244ff53d86318410faadb16
Saqib Tanveer <adward797@gmail.com>
2023-09-29 01:05:09 -0700

    try to fixing submit message error

--- app/api/sendemail/route.js
+++ app/api/sendemail/route.js
@@ -1,21 +1,58 @@
-import { EmailTemplate } from '@/components/EmailTemplate';
 import { NextResponse } from 'next/server';
-import { Resend } from 'resend';
 
-const resend = new Resend(process.env.RESEND_API);
+export const runtime = 'edge';
+export const dynamic = 'force-dynamic';
 
 export async function POST(request) {
     try {
         const { name, email, message } = await request.json();
-        const data = await resend.emails.send({
-            from: 'Acme <onboarding@resend.dev>',
-            to: ['adward797@gmail.com'],
-            subject: 'saqib_tanveer_',
-            react: EmailTemplate({ name, email, message }),
+        const res = await fetch('https://api.resend.com/emails', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${process.env.RESEND_API}`,
+            },
+            body: JSON.stringify({
+                from: 'Acme <onboarding@resend.dev>',
+                to: ['adward797@gmail.com'],
+                subject: 'saqib_tanveer_',
+                html: `<div>
+                        <h1>From, ${name}!</h1>
+                        <p><strong>email: </strong> ${email}</p>
+                        <p>${message}</p>
+                        </div>`,
+            }),
         });
 
+        const data = await res.json();
         return NextResponse.json({ message: 'Message Recieved Successfully', status: true, data }, { status: 200 });
+
     } catch (error) {
         return NextResponse.json({ message: 'Internal Server Error', status: false, error: error.message }, { status: 500 });
     }
 }
+
+
+
+
+// import { EmailTemplate } from '@/components/EmailTemplate';
+// import { NextResponse } from 'next/server';
+// import { Resend } from 'resend';
+
+// const resend = new Resend(process.env.RESEND_API);
+
+// export async function POST(request) {
+//     try {
+//         const { name, email, message } = await request.json();
+//         const data = await resend.emails.send({
+//             from: 'Acme <onboarding@resend.dev>',
+//             to: ['adward797@gmail.com'],
+//             subject: 'saqib_tanveer_',
+//             react: EmailTemplate({ name, email, message }),
+//         });
+
+//         return NextResponse.json({ message: 'Message Recieved Successfully', status: true, data }, { status: 200 });
+//     } catch (error) {
+//         return NextResponse.json({ message: 'Internal Server Error', status: false, error: error.message }, { status: 500 });
+//     }
+// }
